c0cb8c1cbe45bc7c0ee1b7fdf05f26b923a31e39
Animesh Banerjee <stant581@gmail.com>
2020-07-21 23:09:46 +0530

    adding the header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+    <div className="app__header">
+      <img className='app__header__image' src='https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png' alt='insta'></img>
+    </div>
+    <h1> Hey there <span role='img'>🙌</span></h1>
     </div>
   );
 }
